ef70840cf5a5bb1273fa962579dd093bf2f8d0e5
Bruno T. Bagesteiro <brunobagesteiro1@hotmail.com>
2019-12-02 20:41:41 -0300

    Fazendo sozinho

---
+++ Meu projeto sozinho/Meu projeto sozinho/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meu_projeto_sozinho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo galera");
+
+            Console.WriteLine("tchau galera");
+
+            Console.ReadKey(true);
+        }
+    }
+}
