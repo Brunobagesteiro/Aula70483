fd0b84e57fa13d0c4278eda76c79b76a28c03bbc
Bruno T. Bagesteiro <brunobagesteiro1@hotmail.com>
2019-12-02 22:08:56 -0300

    exercicios de carros

---
+++ listaDeCarro/listaDeCarro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaDeCarro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> minhaListaDeCarro = new List<string>
+            {
+                "combi",
+                "deurey",
+                "fusca",
+                "belina",
+                "chevet",
+
+
+            };
+
+
+        }
+    }
+}
