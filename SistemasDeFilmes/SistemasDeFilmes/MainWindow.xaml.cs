de991622db1b482485dacf91f04b03d3b273ed08
Bruno T. Bagesteiro <brunobagesteiro1@hotmail.com>
2019-12-05 22:19:06 -0300

    Aula  4     05/12

---
+++ SistemasDeFilmes/SistemasDeFilmes/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SistemasDeFilmes
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dataGrid.ItemsSource = ListaDeFilmes;
+        }
+        List<string> ListaDeFilmes = new List<string>
+        {
+          "Rei Leão",
+          "Nova Era",
+          "A Casa Caiu",
+          "A mumia",
+          "O Sopro"
+        };
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = null;
+
+            Pesquisa(tbxFiltro.Text);
+        }
+        private void Pesquisa(string parametro)
+        {
+            dataGrid.ItemsSource = ListaDeFilmes.Where(x => x.Contains(parametro));
+
+        }
+
+    }
+}
---
+++ iniciandoWPF/iniciandoWPF/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace iniciandoWPF
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dataGrid.ItemsSource = ListaDeNomes;
+        }
+
+        List<string> ListaDeNomes = new List<string>
+        {
+            "Chael",
+            "Rocneth",
+            "Liamcyn",
+            "Dicjohn",
+            "Dahe",
+            "Chellren",
+            "Kimni",
+            "Hadod",
+            "Garnyahildi",
+            "Reginisen",
+            "Danmax",
+            "Leysig",
+            "Hesfastnath",
+            "Jenba",
+            "Marmaand",
+            "Gorma",
+            "Helm",
+            "Isumlin",
+            "Jenann",
+            "Doegar"
+        };
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = null;
+
+            var ItemSelecionado = cbxFiltro.SelectedIndex;
+
+            switch (cbxFiltro.SelectedIndex)
+            {
+                case 0:
+                    OrdenarList();
+                    break;
+                case 1:
+                    FiltraPorLetras(TbxFiltro.Text);
+                    break;
+                case 2:
+                    OrdenarMaiorParaMenor();
+                    break;
+            }
+        }
+
+        private void OrdenarList()
+        {
+            dataGrid.ItemsSource = ListaDeNomes.OrderBy(x => x);
+        }
+
+        private void OrdenarMaiorParaMenor()
+        {
+            dataGrid.ItemsSource = ListaDeNomes.OrderByDescending (x => x.Length);  
+
+        }
+
+
+
+        private void FiltraPorLetras(string parametro)
+        {
+            dataGrid.ItemsSource = ListaDeNomes.Where(x => x.Contains(parametro));
+        }
+    }
+}
