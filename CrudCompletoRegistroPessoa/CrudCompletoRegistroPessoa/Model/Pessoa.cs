fef49da48b50ff68857c5f6a8581b3700f1f0729
Bruno T. Bagesteiro <brunobagesteiro1@hotmail.com>
2019-12-12 22:10:07 -0300

    Aula 12/12/19

---
+++ CrudCompletoRegistroPessoa/CrudCompletoRegistroPessoa/Model/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudCompletoRegistroPessoa.Model
+{
+   public class Pessoa
+    {
+        [Key]
+
+        public int ID { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+    }
+}
