813a11d1e5a63c30da6c12259c75141f7cc4f8bb
Bruno T. Bagesteiro <brunobagesteiro1@hotmail.com>
2019-12-16 22:16:50 -0300

    Penultima aula 16/12/2019

---
+++ SistemaControleEstoque/SistemaControleEstoque/Model/SistemaContext.cs
@@ -0,0 +1,33 @@
+namespace SistemaControleEstoque.Model
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class SistemaContext : DbContext
+    {
+        // Your context has been configured to use a 'Sistema' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'SistemaControleEstoque.Model.Sistema' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'Sistema' 
+        // connection string in the application configuration file.
+        public SistemaContext()
+            : base("name=Sistema")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+        
+         public virtual DbSet<Usuario> Usuarios { get; set; }
+
+        public virtual DbSet<Estoque> Estoques { get; set; }
+    }
+}
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
---
+++ SistemaControleEstoque/SistemaControleEstoque/View/CasdastrarUsuario.xaml.cs
@@ -0,0 +1,50 @@
+﻿using SistemaControleEstoque.Controller;
+using SistemaControleEstoque.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SistemaControleEstoque.View
+{
+    /// <summary>
+    /// Lógica interna para CasdastrarUsuario.xaml
+    /// </summary>
+    public partial class CasdastrarUsuario : Window
+    {
+        public CasdastrarUsuario()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(tbxSenha.Password != tbxConfirmar.Password)
+            {
+                MessageBox.Show("As senhas não conferem!");
+                return;
+            }
+
+
+
+            new UsuarioController().CadastrarUsuario(new Usuario()
+            {
+                Login = tbxLogin.Text,
+                Senha = tbxSenha.Password
+            });
+
+            DialogResult = true;
+        }
+    }
+}
